51d9d58602d94f4091fbd2727a0068deeb00c877
Liz P <parisielizabeth@gmail.com>
2021-03-14 00:24:20 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -14,3 +14,9 @@ function reducer(state, action) {
 function dispatch(action) {
   state = reducer(state, action);
 }
+
+function render() {
+  let container = document.getElementById('container');
+  
+  container.textContent = state.count;
+}
